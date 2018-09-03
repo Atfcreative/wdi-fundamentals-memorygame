d84a06389ff1e4d72e2daeda96a6f82e953e309d
Andrew Foster <andrewfoster@imac-de-andrew.attlocal.net>
2018-09-03 11:33:56 -0700

    game logic created

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,10 +1,18 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardTwo);
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
 
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
 
+if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!");
+          
+    } else {
+          alert("Sorry, try again");
+}
 
