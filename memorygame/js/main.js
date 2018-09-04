4c585d55973764c55cdc05b8ac5309847394bc68
Andrew Foster <andrewfoster@imac-de-andrew.attlocal.net>
2018-09-04 12:55:56 -0700

    Game logic is functional

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,44 +1,96 @@
 var cards = [
 {
-rank: 'queen',
-suit: 'hearts',
-cardImage: 'images/queen-of-hearts.png'
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
 },
 {
-rank: 'queen',
-suit: 'diamonds',
-cardImage: 'images/queen-of-diamonds.png'
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
 },
 {
-rank: 'king',
-suit: 'hearts',
-cardImage: 'images/king-of-hearts.png'
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
 },
 {
-rank: 'king',
-suit: 'hearts',
-cardImage: 'images/king-of-diamonds.png'
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-diamonds.png"
 }
 ];
 
 var cardsInPlay = [];
 
-var checkForMath = function () {
+function checkForMatch() {
+	// Use the setAttribute method to update the src 
+	//attribute to the image of the card that was just clicked 
+	//this.setAttribute('src', cards[cardId].cardImage);
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	          alert("You found a match!");
-	          
-	    } else {
-	          alert("Sorry, try again");
-	}
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+}
 }
 
-var flipCard = function (cardId) {
-	checkForMath();
+
+function flipCard() {
+
+var cardId = this.getAttribute('data-id')
+
+  cardsInPlay.push(cards[cardId].rank);
+
+  this.setAttribute('src', cards[cardId].cardImage);
+
+  if (cardsInPlay.length === 2) {
+
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  	alert("You found a match!") 
+  }
+	else if (cardsInPlay[0] !== cardsInPlay[1]) {
+   	alert("Sorry, try again!")
+   }
+  	return flipCard;
 }
 
-cardsInPlay.push(cards[0].rank);
-cardsInPlay.push(cards[2].rank);
 
+
+ console.log("You Flipped " + cards[cardId].rank)
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suit)
+
+ checkForMatch();
+ /*cards[cardId] references the entire  object in the cards array
+ cards[cardId].ranks checks within the object.rank*/
+}
+
+function createBoard() {
+  for(var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
+}
+function pageReset() {
+  document.getElementById('reset-button');
+}
+
+
+
+createBoard();
+
+pageReset();
+
+
+
+
+
+/*
 console.log("User flipped " + cards[0].rank);
 console.log(cards[0].cardImage);
 console.log(cards[0].suit);
@@ -47,13 +99,6 @@ console.log("User flipped " + cards[2].rank);
 console.log(cards[2].cardImage);
 console.log(cards[2].suit);
 
-
-flipCard(0);
-flipCard(2);
-
-
-
-/*
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
@@ -62,3 +107,17 @@ var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
 */
+
+/*
+console.log("User flipped " + cards[0].rank);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
+
+console.log("User flipped " + cards[2].rank);
+console.log(cards[2].cardImage);
+console.log(cards[2].suit);
+*/
+
+
+//flipCard(0);
+//flipCard(2);
